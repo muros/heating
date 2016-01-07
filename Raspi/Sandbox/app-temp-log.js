d2deaaf5b78e46e0886ff59f4ead0c93afe88445
Uros <upumesar@gmail.com>
2016-01-07 22:34:44 +0100

    Added support for two way communication arduino - node, so that temp and state can be set from node and not just temp read from arduino.

--- Raspi/Sandbox/app-temp-log.js
+++ Raspi/Sandbox/app-temp-log.js
@@ -5,6 +5,11 @@ var express = require('express');
 var serialPort = require("serialport");
 var SerialPort = serialPort.SerialPort;
 
+var lowTemp = 17.0;
+var highTemp = 22.0;
+var currTemp = 17.0;
+var thermState = -1;
+
 var arduinoPortName;
 var arduinoPort;
 serialPort.list(function (err, ports) {
@@ -40,29 +45,59 @@ process.stdin.on('data', function (text) {
           parser: serialPort.parsers.readline("\n")
         });
     arduinoPort.on("data", function (data) {
+      console.log('DATA: ' +  data);
       var termostat = JSON.parse(data);
       var date = new Date();
       var time = date.toISOString();
       var temp = termostat.temp;
       var hmdt = termostat.hmdt;
+      var state = termostat.state;
 
       db.run("INSERT into temperature(time, temp, hmdt) VALUES ('" + time + "', "
           + temp + ", " +  hmdt + ")");
+
+      if ((thermState != -1) && (thermState != state)) {
+        state = thermState;
+        thermState = -1;
+      }
+
+      //var str = "1 17.0 22.5 21.5\n";
+      var str = cmdToThermostat(state, lowTemp, highTemp, currTemp);
+      var bytes = [];
+      for (var i = 0; i < str.length; ++i) {
+        bytes.push(str.charCodeAt(i));
+      }
+      arduinoPort.write(bytes, function(err, results) {
+        console.log('ack');
+      });
     });
-
-
+  } else if (text === 'night\n') {
+    thermState = 1;
+  } else if (text === 'day\n') {
+    thermState = 2;
+  } else if (text === 'timer\n') {
+    thermState = 3;
+  } else if (text === 'burst\n') {
+    thermState = 4;
+  } else if (text === 'off\n') {
+    thermState = 0;
   } else {
-    info = text.replace(/(\r\n|\n|\r)/gm,"");
-  
-    var date = new Date();
-    var time = date.toISOString();
-    var temp = Math.random()*30;
-    var hmdt = Math.random()*100;
-
-    db.run("INSERT into temperature(time, temp, hmdt) VALUES ('" + time + "', " 
-      + temp + ", " +  hmdt + ")");
+    console.log("Unknown command.")
   }
 });
+
+/**
+ * Create command string that is sent to Arduino controller - thermostat.
+ * @param state state can be 0 - 4
+ * @param lowTemp night temperature in state 1
+ * @param highTemp high temperature in state 2
+ * @param currTemp any temperature in state 3
+ */
+function cmdToThermostat(state, lowTemp, highTemp, currTemp) {
+  var cmdStr = "" + state + " " + lowTemp + " " + highTemp + " " + currTemp + "\n";
+
+  return cmdStr;
+}
 
 function done() {
   console.log('Now that process.stdin is paused, there is nothing more to do.');
@@ -100,21 +135,6 @@ app.get('/temp', function (req, res) {
     res.contentType('text/json; charset=UTF-8');
     res.send(temps);
   });
-  /*
-  db.each("SELECT time, temp, hmdt FROM temperature", function(err, row) {
-    var d = new Date(row.time);
-    var temp = [];
-    var date = [];
-    date[0] = d.getHours();
-    date[1] = d.getMinutes();
-    date[2] = d.getSeconds();
-    temp[0] = date;
-    temp[1] = row.temp;
-    temp[2] = row.hmdt;
-    temps[i++] = temp;
-    console.log(temp);
-  });
-  */
 });
 
 app.use(express.static('public'));
